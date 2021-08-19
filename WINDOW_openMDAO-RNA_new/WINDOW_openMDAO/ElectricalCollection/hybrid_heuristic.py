faff44e2729a24d1e73f90c855149e5f5a536de0
mihir0210 <mihir0210@gmail.com>
2021-08-19 20:22:43 +0200

    Updated infield cable selection

--- WINDOW_openMDAO-RNA_new/WINDOW_openMDAO/ElectricalCollection/hybrid_heuristic.py
+++ WINDOW_openMDAO-RNA_new/WINDOW_openMDAO/ElectricalCollection/hybrid_heuristic.py
@@ -11,9 +11,10 @@ def choose_cables(number_turbines_per_cable, turbine_rated_current):
             if turbine_rated_current * number <= cable[1]:
                 cable_list.append([number, cable[2] + 365.0])
                 break
-            else:
+            elif turbine_rated_current * number > cables_info[-1][1]:
                 cable_list.append([number, 579 + 365.0])
                 break
+
     print cable_list
     return cable_list
 
